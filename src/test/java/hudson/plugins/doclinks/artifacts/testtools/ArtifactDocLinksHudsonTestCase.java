b5a7ca57683da9e274a36b3511532f99fe0fccfd
ikedam <devld@ikedam.jp>
2013-09-08 13:31:24 +0900

    [JENKINS-19456] Added tests for publishing artifacts as documents.

---
+++ src/test/java/hudson/plugins/doclinks/artifacts/testtools/ArtifactDocLinksHudsonTestCase.java
@@ -0,0 +1,69 @@
+/*
+ * The MIT License
+ * 
+ * Copyright (c) 2013 IKEDA Yasuyuki
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package hudson.plugins.doclinks.artifacts.testtools;
+
+import org.jvnet.hudson.test.HudsonTestCase;
+
+import hudson.model.AbstractProject;
+
+/**
+ *
+ */
+public abstract class ArtifactDocLinksHudsonTestCase extends HudsonTestCase {
+    private WebClient aWebClient = null;
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        aWebClient = null;
+    }
+    
+    /**
+     * Singleton for {@link WebClient}.
+     * 
+     * There seems no way to have {@link WebClient} close the connection.
+     * This causes Jenkins exhaust its connection pools
+     * when creating {@link WebClient} for each assertion step.
+     * 
+     * @return
+     */
+    protected synchronized WebClient getWebClient() {
+        if (aWebClient == null) {
+            aWebClient = createWebClient();
+        }
+        return aWebClient;
+    }
+    
+    public WebClient createWebClient() {
+        WebClient wc = new WebClient();
+        wc.setTimeout(3000);
+        return wc;
+    }
+    
+    protected void updateTransientActions(AbstractProject<?,?> p) throws Exception {
+        WebClient wc = getWebClient();
+        submit(wc.getPage(p, "configure").getFormByName("config"));
+    }
+}
---
+++ src/test/java/hudson/plugins/doclinks/artifacts/testtools/TestZipBuilder.java
@@ -0,0 +1,119 @@
+/*
+ * The MIT License
+ * 
+ * Copyright (c) 2013 IKEDA Yasuyuki
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package hudson.plugins.doclinks.artifacts.testtools;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.tasks.Builder;
+
+/**
+ * Creates a zip file with specified name.
+ */
+public class TestZipBuilder extends Builder
+{
+    private String filename;
+    
+    public TestZipBuilder(String filename) {
+        this.filename = filename;
+    }
+    
+    @Override
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher,
+            BuildListener listener) throws InterruptedException, IOException {
+        FilePath file = build.getWorkspace().child(filename);
+        FilePath dir = file.getParent();
+        
+        if (dir != null && !dir.exists()) {
+            dir.mkdirs();
+        }
+        
+        String seperator = System.getProperty("file.separator");
+        String resourceDirName = StringUtils.join(getClass().getName().split("\\."), seperator);
+        File resourceDir = null;
+        try {
+           resourceDir = new File(ClassLoader.getSystemResource(resourceDirName).toURI());
+        } catch(URISyntaxException e) {
+           listener.getLogger().println("Failed to retrieve contents to zip");
+           e.printStackTrace(listener.getLogger());
+        }
+        
+        OutputStream os = null;
+        ZipOutputStream zos = null;
+        
+        try {
+            os = file.write();
+            zos = new ZipOutputStream(os);
+            
+            compress(zos, resourceDir, null);
+        } finally {
+            if (zos != null) {
+                zos.close();
+            }
+            
+            if (os != null) {
+                os.close();
+            }
+        }
+        
+        return true;
+    }
+    
+    private void compress(ZipOutputStream zos, File dir, String relative) throws IOException {
+        for (String filename: dir.list()) {
+            File file = new File(dir, filename);
+            String path = StringUtils.isEmpty(relative)?filename:String.format("%s/%s", relative, filename);
+            if (file.isDirectory()) {
+                ZipEntry entry = new ZipEntry(String.format("%s/", path));
+                zos.putNextEntry(entry);
+                compress(zos, file, path);
+            } else {
+                ZipEntry entry = new ZipEntry(path);
+                zos.putNextEntry(entry);
+                FileInputStream is = null;
+                try {
+                    is = new FileInputStream(file);
+                    IOUtils.copy(is, zos);
+                } finally {
+                    if (is != null) {
+                        is.close();
+                    }
+                }
+            }
+        }
+    }
+}
